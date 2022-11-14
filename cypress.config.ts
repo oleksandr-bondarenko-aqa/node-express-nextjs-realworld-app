b8ed822e1901ba70cfad7bc78e54f26578ab0c48
Oleksandr Bondarenko <oleksandr.bondarenko.qa@gmail.com>
2022-11-14 21:28:52 +0200

    fix dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  watchForFileChanges: false,
   e2e: {
-    viewportHeight: 1080,
-    viewportWidth: 1920,
-    watchForFileChanges: false,
     baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
